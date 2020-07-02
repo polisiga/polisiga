efb4cd9500ffacfbd56ec2de69ad7eef58d8f9d8
Guillermo Silva <guillermitus@gmail.com>
2020-07-02 14:16:12 -0400

    autenticacion

--- polisiga/urls.py
+++ polisiga/urls.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import include
 from django.urls import path
 
+
+
 urlpatterns = [
     path('academico',include('academico.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
+
+
+'''
+accounts/login/ [name='login']
+accounts/logout/ [name='logout']
+accounts/password_change/ [name='password_change']
+accounts/password_change/done/ [name='password_change_done']
+accounts/password_reset/ [name='password_reset']
+accounts/password_reset/done/ [name='password_reset_done']
+accounts/reset/<uidb64>/<token>/ [name='password_reset_confirm']
+accounts/reset/done/ [name='password_reset_complete']
+'''
