3b73414e9a26205a0a85be800f5753a65909586a
Guillermo Silva <guillermitus@gmail.com>
2020-08-26 16:30:52 -0400

    ajustes de estilo

--- academico/admin.py
+++ academico/admin.py
@@ -1,8 +1,10 @@
+"""Pagina administrativa"""
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.contrib.auth.models import User
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
+
 
 from .models import (
     Alumno,
@@ -24,76 +26,104 @@ from .forms import (
     CatedraForm
 )
 
+
+User = get_user_model()
+
+
 class DocenteInline(admin.StackedInline):
+    """Docstring"""
     model = Docente
     can_delete = False
 
+
 class UserAdmin(BaseUserAdmin):
+    """Docstring"""
     inlines = [DocenteInline]
+
 
 admin.site.unregister(User)
 
 admin.site.register(User, UserAdmin)
 
+
 class AlumnoResource(resources.ModelResource):
+    """Docstring"""
     class Meta:
+        """Docstring"""
         model = Alumno
 
+
 class AlumnoAdmin(ImportExportModelAdmin):
+    """Docstring"""
     resource_class = AlumnoResource
+
 
 admin.site.register(Alumno, AlumnoAdmin)
 
+
 class AsignaturaResource(resources.ModelResource):
+    """Docstring"""
     class Meta:
+        """Docstring"""
         model = Asignatura
+
 
 @admin.register(Asignatura)
 class AsignaturaAdmin(ImportExportModelAdmin):
-    list_display = ('id', 'codigo','grupohomologas','siglas','nombre','carrera', 'departamento')
-    search_fields = ('id', 'codigo','siglas','nombre')
+    """Docstring"""
+    list_display = (
+        'id', 'codigo', 'grupohomologas',
+        'siglas', 'nombre', 'carrera', 'departamento')
+    search_fields = ('id', 'codigo', 'siglas', 'nombre')
     resource_class = AsignaturaResource
+
 
 @admin.register(Carrera)
 class CarreraAdmin(ImportExportModelAdmin):
-    pass
+    """Docstring"""
+
 
 @admin.register(Catedra)
 class CatedraAdmin(ImportExportModelAdmin):
+    """Docstring"""
     form = CatedraForm
 
     autocomplete_fields = [
         'docentes',
-        'asignaturas'
-        
+        'asignaturas',
     ]
 
-    def get_form(self, request, obj = None, **kwargs):
+    def get_form(self, request, obj=None, **kwargs):
         form = super(CatedraAdmin, self).get_form(request, **kwargs)
-        #qs = Asignatura.objects.filter(pk=3)
-        #form.base_fields['asignaturas'].queryset = qs
+        # qs = Asignatura.objects.filter(pk=3)
+        # form.base_fields['asignaturas'].queryset = qs
         return form
+
 
 @admin.register(Contenido)
 class ContenidoAdmin(ImportExportModelAdmin):
-    pass
+    """Docstring"""
+
 
 @admin.register(Departamento)
 class DepartamentoAdmin(ImportExportModelAdmin):
-    pass
+    """Docstring"""
+
 
 @admin.register(Docente)
 class DocenteAdmin(ImportExportModelAdmin):
+    """Docstring"""
     search_fields = [
         'nombre',
         'apellido'
     ]
     autocomplete_fields = [
         'user',
-        
     ]
 
+
 class AsignaturaInLine(admin.TabularInline):
+    """Docstring"""
     model = Asignatura
     fields = [
         'id',
@@ -119,25 +149,33 @@ class AsignaturaInLine(admin.TabularInline):
     def has_add_permission(self, request, obj):
         return False
 
+
 @admin.register(Enfasis)
 class EnfasisAdmin(ImportExportModelAdmin):
-    pass
+    """Docstring"""
+
 
 @admin.register(GrupoHomologas)
 class GrupoHomologasAdmin(ImportExportModelAdmin):
+    """Docstring"""
     inlines = [
         AsignaturaInLine,
     ]
 
+
 @admin.register(Periodo)
 class PeriodoAdmin(ImportExportModelAdmin):
-    pass
+    """Docstring"""
+
 
 class ContenidoInLine(admin.TabularInline):
+    """Docstring"""
     model = Contenido
+
 
 @admin.register(Plan)
 class PlanAdmin(ImportExportModelAdmin):
+    """Docstring"""
     autocomplete_fields = [
         'asignatura',
     ]
@@ -145,6 +183,7 @@ class PlanAdmin(ImportExportModelAdmin):
         ContenidoInLine
     ]
 
+
 @admin.register(RegistroCatedra)
 class RegistroCatedraAdmin(ImportExportModelAdmin):
-    pass
+    """Docstring"""
